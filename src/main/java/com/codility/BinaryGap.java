6df69889365123f8284e0c13b817ab2c0cdfcd42
Lukasz184396 <zawadalukasz88@gmail.com>
2018-07-20 21:32:47 +0200

    BinaryGap in Java 8 Refactored

--- src/main/java/com/codility/BinaryGap.java
+++ src/main/java/com/codility/BinaryGap.java
@@ -1,24 +1,17 @@
 package com.codility;
 
+import java.util.stream.Stream;
+
 public class BinaryGap {
 
     public int solution(int integerNumber) {
-        int longestBinaryGap = 0;
-        int numberElementToCheck;
-        String[] tab;
-        String binaryNumberString = Integer.toBinaryString(integerNumber);
-        if (binaryNumberString.charAt(binaryNumberString.length() - 1) == '0') {
-            tab = binaryNumberString.split("1");
-            numberElementToCheck = tab.length - 1;
-        } else {
-            tab = binaryNumberString.split("1");
-            numberElementToCheck = tab.length;
-        }
-        for (int i = 0; i < numberElementToCheck; i++) {
-            int tempLongestBinaryGap = tab[i].length();
-            if (tempLongestBinaryGap > longestBinaryGap)
-                longestBinaryGap = tempLongestBinaryGap;
-        }
-        return longestBinaryGap;
+        return Stream
+                .of(Integer.toBinaryString(integerNumber)
+                .replaceAll("0+$", "")
+                .split("1+"))
+                .filter(a -> a != null)
+                .map(String::length)
+                .max(Integer::compare)
+                .orElse(0);
     }
 }
