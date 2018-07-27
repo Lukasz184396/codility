5d6ffdc4516c95a1d76dd0e31e35e432da27eb72
Lukasz184396 <zawadalukasz88@gmail.com>
2018-07-27 20:38:56 +0200

    Added next test to check the solution is proprer

--- src/test/java/com/codility/OddOccurrencesInArrayTest.java
+++ src/test/java/com/codility/OddOccurrencesInArrayTest.java
@@ -12,4 +12,22 @@ public class OddOccurrencesInArrayTest {
         assertEquals(7, new  OddOccurrencesInArray().solution(array));
     }
 
+    @Test
+    public void shouldReturnSevenWhenOddIsInTheBeginingOfArray() {
+        int [] array = {7, 9,3,9,3,9,9};
+        assertEquals(7, new  OddOccurrencesInArray().solution(array));
+    }
+
+    @Test
+    public void shouldReturnSevenWhenOddIsInTheEndOfArray() {
+        int [] array = {7,9,3,9,3,9,9};
+        assertEquals(7, new  OddOccurrencesInArray().solution(array));
+    }
+
+    @Test
+    public void shouldReturnSevenWhenIsSingleElementInTheArray() {
+        int [] array = {7};
+        assertEquals(7, new  OddOccurrencesInArray().solution(array));
+    }
+
 }
