7f696eea385130ed827d1b1d4ccfe624fe4def52
Lukasz184396 <zawadalukasz88@gmail.com>
2018-07-30 22:09:52 +0200

    Removed another unneeded initializaion of variable

--- src/main/java/com/codility/FrogJmp.java
+++ src/main/java/com/codility/FrogJmp.java
@@ -3,9 +3,8 @@ package com.codility;
 public class FrogJmp {
 
     int solution(int start, int finish, int jump) {
-        int position = start;
         int result = (finish-start)/jump;
-        position = position + result * jump;
+        int position = start + result * jump;
         if(position<finish) result++;
         return result;
     }
