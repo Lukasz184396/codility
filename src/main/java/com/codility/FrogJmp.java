ab9b4e8519c54986b516869a84777f0ea3c8c30e
Lukasz184396 <zawadalukasz88@gmail.com>
2018-07-30 22:06:58 +0200

    Deleted not needed inicialization of result variable

--- src/main/java/com/codility/FrogJmp.java
+++ src/main/java/com/codility/FrogJmp.java
@@ -3,9 +3,8 @@ package com.codility;
 public class FrogJmp {
 
     int solution(int start, int finish, int jump) {
-        int result = 0;
         int position = start;
-        result = (finish-start)/jump;
+        int result = (finish-start)/jump;
         position = position + result * jump;
         if(position<finish) result++;
         return result;
