580108f198525e592b15787ea9e8d35acf14a40c
Lukasz184396 <zawadalukasz88@gmail.com>
2018-07-31 21:01:30 +0200

    Added solution passing first test

--- src/main/java/com/codility/PermMissingElem.java
+++ src/main/java/com/codility/PermMissingElem.java
@@ -2,6 +2,11 @@ package com.codility;
 
 public class PermMissingElem {
     public int solution(int[] array) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        int sumOfNaturalNumbers = (array.length+1) * (array.length +2) / 2;
+        return sumOfNaturalNumbers - sum;
     }
 }
