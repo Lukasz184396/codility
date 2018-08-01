76d167f197a697077b632060a38edfcd0daa6450
Lukasz184396 <zawadalukasz88@gmail.com>
2018-08-01 20:36:08 +0200

    Passing first test

--- src/main/java/com/codility/TapeEquilibrium.java
+++ src/main/java/com/codility/TapeEquilibrium.java
@@ -1,7 +1,26 @@
 package com.codility;
 
+
 public class TapeEquilibrium {
-    public int solution(int[] array) {
-        return 0;
+    public int solution(int[] A) {
+
+        long firstSum = A[0];
+        long secondSum = 0;
+
+        for (int i =1;i<A.length;i++)
+            secondSum += A[i];
+
+        long minDifferenceBetweenSums = Math.abs(Math.abs(secondSum)+Math.abs(firstSum));
+
+        for (int indexOfSeparation=1; indexOfSeparation<A.length; indexOfSeparation++)
+        {
+            if ( Math.abs(Math.abs(firstSum) - Math.abs(secondSum)) < minDifferenceBetweenSums )
+                minDifferenceBetweenSums = Math.abs( Math.abs(firstSum) - Math.abs(secondSum) );
+            firstSum += A[indexOfSeparation];
+            secondSum -=A[indexOfSeparation];
+        }
+        return (int) minDifferenceBetweenSums;
     }
+
 }
+
