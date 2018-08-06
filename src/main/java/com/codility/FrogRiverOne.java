39dceee3a184a998c11930bb2da731c10c00a365
Lukasz184396 <zawadalukasz88@gmail.com>
2018-08-06 22:09:43 +0200

    Added test for FrogRiverOne and impl not passing this test

---
+++ src/main/java/com/codility/FrogRiverOne.java
@@ -0,0 +1,7 @@
+package com.codility;
+
+public class FrogRiverOne {
+    public int solution(int X, int[] array) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/codility/FrogRiverOneTest.java
@@ -0,0 +1,13 @@
+package com.codility;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FrogRiverOneTest {
+    @Test
+    public void firsttest() {
+        int[] array = {1,3,1, 4, 2, 3, 5, 4};
+        assertEquals(6,new FrogRiverOne().solution(3, array ));
+    }
+}
