a5d39cd9ac24f061339330f9af7c0a2dd923b6ed
Lukasz184396 <zawadalukasz88@gmail.com>
2018-08-06 22:22:54 +0200

    Added impl of FrogRiverOne passing first test

--- src/main/java/com/codility/FrogRiverOne.java
+++ src/main/java/com/codility/FrogRiverOne.java
@@ -1,7 +1,18 @@
 package com.codility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FrogRiverOne {
     public int solution(int X, int[] array) {
-        return 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < X; i++) {
+            list.add(i+1);
+        }
+        for (int i = 0; i < array.length; i++) {
+            list.remove(Integer.valueOf(array[i]));
+            if(list.isEmpty()) return i;
+        }
+        return -1;
     }
 }
--- src/test/java/com/codility/FrogRiverOneTest.java
+++ src/test/java/com/codility/FrogRiverOneTest.java
@@ -8,6 +8,6 @@ public class FrogRiverOneTest {
     @Test
     public void firsttest() {
         int[] array = {1,3,1, 4, 2, 3, 5, 4};
-        assertEquals(6,new FrogRiverOne().solution(3, array ));
+        assertEquals(6,new FrogRiverOne().solution(5, array ));
     }
 }
