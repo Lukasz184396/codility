8a1003b853f2abda414cfb9be0e2e7246a16d51d
Lukasz184396 <zawadalukasz88@gmail.com>
2018-08-06 22:27:13 +0200

    Added test with negative value

--- src/test/java/com/codility/FrogRiverOneTest.java
+++ src/test/java/com/codility/FrogRiverOneTest.java
@@ -6,8 +6,13 @@ import static org.junit.Assert.assertEquals;
 
 public class FrogRiverOneTest {
     @Test
-    public void firsttest() {
+    public void testPositive() {
         int[] array = {1,3,1, 4, 2, 3, 5, 4};
         assertEquals(6,new FrogRiverOne().solution(5, array ));
+    }
+    @Test
+    public void testNegative() {
+        int[] array = {1,3,1, 4, 2, 3, 4, 4};
+        assertEquals(-1,new FrogRiverOne().solution(5, array ));
     }
 }
