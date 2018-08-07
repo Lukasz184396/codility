a4a08b639bb1fe6aa049b4e413b550c085767903
Lukasz184396 <zawadalukasz88@gmail.com>
2018-08-07 22:13:53 +0200

    Revert "Modified good version on bed to test collections solutions"
    This reverts commit 40d3028a4599629b341507dbdfdac87c674d6be9.

--- src/main/java/com/codility/MissingInteger.java
+++ src/main/java/com/codility/MissingInteger.java
@@ -1,20 +1,13 @@
 package com.codility;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class MissingInteger {
     public int solution(int[] array) {
         int smallestIntegerNotOccuringInArray = 1;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            list.add(array[i]);
-        }
-        Collections.sort(list);
-        Set<Integer> set = new HashSet<>(list);
         Arrays.sort(array);
-        Iterator<Integer> it = set.iterator();
-        while(it.hasNext()){
-            if(it.next() == smallestIntegerNotOccuringInArray) {
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == smallestIntegerNotOccuringInArray) {
                 smallestIntegerNotOccuringInArray++;
             }
         }
