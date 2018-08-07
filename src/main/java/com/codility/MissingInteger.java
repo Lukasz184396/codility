40d3028a4599629b341507dbdfdac87c674d6be9
Lukasz184396 <zawadalukasz88@gmail.com>
2018-08-07 22:10:17 +0200

    Modified good version on bed to test collections solutions

--- src/main/java/com/codility/MissingInteger.java
+++ src/main/java/com/codility/MissingInteger.java
@@ -1,13 +1,20 @@
 package com.codility;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class MissingInteger {
     public int solution(int[] array) {
         int smallestIntegerNotOccuringInArray = 1;
-        Arrays.sort(array);
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < array.length; i++) {
-            if(array[i] == smallestIntegerNotOccuringInArray) {
+            list.add(array[i]);
+        }
+        Collections.sort(list);
+        Set<Integer> set = new HashSet<>(list);
+        Arrays.sort(array);
+        Iterator<Integer> it = set.iterator();
+        while(it.hasNext()){
+            if(it.next() == smallestIntegerNotOccuringInArray) {
                 smallestIntegerNotOccuringInArray++;
             }
         }
