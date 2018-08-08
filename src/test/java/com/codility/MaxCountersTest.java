2850d077a2e02cac478f744754aa538d2c3be28e
Lukasz184396 <zawadalukasz88@gmail.com>
2018-08-08 22:19:01 +0200

    Refactored test MaxCounter

--- src/test/java/com/codility/MaxCountersTest.java
+++ src/test/java/com/codility/MaxCountersTest.java
@@ -7,22 +7,11 @@ import static org.junit.Assert.assertEquals;
 public class MaxCountersTest {
     @Test
     public void shouldReturnValuesOfCounters () {
-        int [] A = new int[7];
-        A[0] = 3;
-        A[1] = 4;
-        A[2] = 4;
-        A[3] = 6;
-        A[4] = 1;
-        A[5] = 4;
-        A[6] = 4;
+        int [] A = {3, 4, 4, 6, 1, 4, 4};
         int [] result = {3, 2, 2, 4, 2};
         int [] actual = new MaxCounters().solution(5, A);
-        assertEquals(result[0],actual[0]);
-        assertEquals(result[1],actual[1]);
-        assertEquals(result[2],actual[2]);
-        assertEquals(result[3],actual[3]);
-        assertEquals(result[4],actual[4]);
-
+        for (int i = 0; i < result.length; i++) {
+            assertEquals(result[i],actual[i]);
+        }
     }
-
 }
