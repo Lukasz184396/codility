d4cd0d20206889d0598a3f38bf78fb1bddcadf12
Lukasz184396 <zawadalukasz88@gmail.com>
2018-10-14 12:34:18 +0200

    Added solutions with aditional test to Passing Cars

--- src/main/java/com/codility/PassingCars.java
+++ src/main/java/com/codility/PassingCars.java
@@ -1,20 +1,35 @@
 package com.codility;
 
+import java.util.List;
+
 public class PassingCars {
 
     public int solution(int[] A) {
         int numberOfPairs = 0;
-        int zerosCounter = 0;
-        int onesCounter = 0;
-        if(A.length==1) return 0;
-//        for (int i = 0; i < A.length; i++) {
-//            if(A[i] == 0) zerosCounter++;
-//        }
-//        onesCounter = A.length-zerosCounter;
-//        //numberOfPairs = onesCounter * zerosCounter -1;
-//
-//
-//        if(numberOfPairs>1000_000_000) return -1;
+        int counterOfZeros = 0;
+        for (int i = 0; i <= A.length - 1; i++) {
+            if (A[i] == 1) {
+                numberOfPairs = counterOfZeros + numberOfPairs;
+                if (numberOfPairs > 1000000000) return -1;
+            } else {
+                counterOfZeros++;
+            }
+        }
+        return numberOfPairs;
+    }
+
+    //solution2 to check when will be more than 1000000000 pairs
+    public int solution2(List<Integer> list) {
+        int numberOfPairs = 0;
+        int counterOfZeros = 0;
+        for (int i = 0; i <= list.size() - 1; i++) {
+            if (list.get(i) == 1) {
+                numberOfPairs = counterOfZeros + numberOfPairs;
+                if (numberOfPairs > 1000000000) return -1;
+            } else {
+                counterOfZeros++;
+            }
+        }
         return numberOfPairs;
     }
 }
--- src/test/java/com/codility/PassingCarsTest.java
+++ src/test/java/com/codility/PassingCarsTest.java
@@ -2,28 +2,52 @@ package com.codility;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class PassingCarsTest {
 
     @Test
-    public void shouldReturnFivePairs (){
-        int [] A = {0,1,0,1,1};
+    public void shouldReturnFivePairs() {
+        int[] A = {0, 1, 0, 1, 1};
 //        A[0] = 0;
 //        A[1] = 1;
 //        A[2] = 0;
 //        A[3] = 1;
 //        A[4] = 1;
-        assertEquals(5,new PassingCars().solution(A));
+        assertEquals(5, new PassingCars().solution(A));
     }
+
     @Test
-    public void shouldReturnZeroIfIsOnlyOneElementInArray (){
-        int [] A = {0};
-//        A[0] = 0;
-//        A[1] = 1;
-//        A[2] = 0;
-//        A[3] = 1;
-//        A[4] = 1;
-        assertEquals(0,new PassingCars().solution(A));
+    public void shouldReturnZeroIfIsOnlyOneElementInArray() {
+        int[] A = {0};
+        assertEquals(0, new PassingCars().solution(A));
+    }
+
+    @Test
+    public void checkIfIsToManyPairs() {
+        int [] A = new int[89442];
+        for (int i = 0; i < 89442; i++) {
+            if (i % 2 == 0) A[i] = 0;
+            else A[i] = 1;
+        }
+        assertEquals(-1, new PassingCars().solution(A));
+    }
+
+    //helper test to check If is to many Pairs
+    @Test
+    public void checkIndexWhenIsToManyPairs() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100000000; i++) {
+            if(i % 2 == 0) list.add(0);
+            else list.add(1);
+            if(new PassingCars().solution2(list) ==-1) {
+                System.out.println("Index:" + i);  //limit value of pairs !!!
+                break;
+            }
+        }
+        assertEquals(-1, new PassingCars().solution2(list));
     }
 }
