d8a68f7f179e58a07af06934041fef54770abf8e
Lukasz184396 <zawadalukasz88@gmail.com>
2018-10-19 19:48:54 +0200

    Fixed readability in Trangle solution method

--- src/main/java/com/codility/Triangle.java
+++ src/main/java/com/codility/Triangle.java
@@ -7,7 +7,9 @@ public class Triangle {
         if(A.length<3) return 0;
         Arrays.sort(A);
         for (int i = 2; i < A.length; i++) {
-            if( ( A[i-2] + A[i-1] ) > A[i] ) return 1;
+            if(
+                (A[i-2] + A[i-1]) > A[i]
+            ) return 1;
         }
         return 0;
     }
