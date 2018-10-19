bcd9333bc8c244ffd86452940a7c777bd0d92c23
Lukasz184396 <zawadalukasz88@gmail.com>
2018-10-19 21:21:56 +0200

    Added MaxProductOfThree solution and test containing scenraio with meaninfull negative numbers

--- src/main/java/com/codility/MaxProductOfThree.java
+++ src/main/java/com/codility/MaxProductOfThree.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class MaxProductOfThree {
     public int solution(int[] A) {
         Arrays.sort(A);
-        return A[A.length-1] * A[A.length-2] *A[A.length-3] ;
+        int maxForOnlyPositive = A[A.length-1] * A[A.length-2] *A[A.length-3];
+        int maxWithNegative = A[A.length-1] * A[0] * A[1];
+        return maxForOnlyPositive > maxWithNegative ? maxForOnlyPositive : maxWithNegative;
     }
 }
--- src/test/java/com/codility/MaxProductOfThreeTest.java
+++ src/test/java/com/codility/MaxProductOfThreeTest.java
@@ -16,4 +16,16 @@ public class MaxProductOfThreeTest {
         int[] A = {-3,1,2,-2,5,6};
         assertEquals(60, new MaxProductOfThree().solution(A));
     }
+
+    @Test
+    public void testWithHighResultOfMultiplicationFirstTwoNegativeNumbersDataInput() {
+//        A[0] = -3
+//        A[1] = -10
+//        A[2] = 2
+//        A[3] = -2
+//        A[4] = 5
+//        A[5] = 6
+        int[] A = {-3,-10,2,-2,5,6};
+        assertEquals(180, new MaxProductOfThree().solution(A));
+    }
 }
