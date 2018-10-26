f421ddfaefdfd29fbaacda5b50ee485742551349
Lukasz184396 <zawadalukasz88@gmail.com>
2018-10-26 20:21:13 +0200

    Added passed all tests solution mehtod in Nesting class

--- src/main/java/com/codility/Nesting.java
+++ src/main/java/com/codility/Nesting.java
@@ -1,8 +1,23 @@
 package com.codility;
 
+import java.util.Stack;
+
 public class Nesting {
     public int solution(String s) {
-        if(s.isEmpty()) return 1;
-        return -1;
+        if (s.isEmpty()) return 1;
+        Stack stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ')') {
+                stack.push(s.charAt(i));
+            } else {
+                if(stack.size()!=0) {
+                    stack.pop();
+                } else {
+                    return 0;
+                }
+            }
+        }
+        if(!stack.isEmpty()) return 0;
+        else return 1;
     }
 }
